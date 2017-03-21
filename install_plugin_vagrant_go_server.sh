9cf2d9470790ec1fed9209b0a335c3346e9a4d8c
Stephen Murby <stephen.murby@tryteksystems.co.uk>
2017-03-21 10:19:31 +0000

    Create install_plugin_vagrant_go_server.sh
    Just keeping a note of the bash used to install the plugin jar to the GOCD Server.

---
+++ install_plugin_vagrant_go_server.sh
@@ -0,0 +1,11 @@
+--- Local Machine ---
+cd /Users/stephen.murby/repos/git/autotrader-shippr-task-plugin
+./gradlew jar  
+scp -P 2222 build/libs/autotrader-shippr-task-plugin-0.0.1.jar vagrant@127.0.0.1:.
+
+--- Vagrant Guest ---
+vagrant up
+vagrant ssh
+rm /var/lib/go-server/plugins/external/autotrader-shippr-task-plugin-0.0.1.jar
+mv ~/autotrader-shippr-task-plugin-0.0.1.jar /var/lib/go-server/plugins/external/
+/etc/init.d/go-server restart
